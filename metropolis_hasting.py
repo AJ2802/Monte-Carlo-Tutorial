9dca1a07cfb3ae9ed08e98885ed1e66fd9a1f8fd
AJ2802 <punwai.tong@gmail.com>
2017-08-23 09:24:45 -0700

    ver2
    Fix MH algorithm

--- metropolis_hasting.py
+++ metropolis_hasting.py
@@ -11,12 +11,21 @@ import matplotlib.pyplot as plt
 from six.moves import cPickle as pickle
 from six.moves import range
 import time
+import math
 
-
+def cnr(n,r):
+    numerator = 1;
+    denominator= 1;
+    for i in range(n, r+1, -1):
+        numerator = numerator*i
+    for j in range(r,1,-1):
+        denominator = denominator*j
+    
+    return numerator/denominator
 class configuration():
     def __init__(self, config):
         if isinstance(config, np.ndarray) and len(config.shape)==1:
-            self.config = configz
+            self.config = config
             self.length = len(config)
         elif isinstance(config, configuration) and len(config.config.shape)==1:
             self.config = config.config
@@ -34,19 +43,19 @@ class configuration():
 
 # function you want to minimize
 def target_prob(configObj):
-    #E is the function that is minimized and the minimium point is optimal point and the minimum value is 0
-    #users is welcomed to modify their favorite function
     n = configObj.length
     x = configObj.config
-    energy = np.sum([x[i]*x[n-i-1] for i in range(n)])+ np.sum([i*x[i] for i in range(n)])
-    pi = np.exp(-energy) #if discrete
+    #energy = np.sum([x[i]*x[n-i-1] for i in range(n)])+ np.sum([i*x[i] for i in range(n)])
+    #pi = np.exp(-energy) #if discrete
     #pi = np.exp(-energy**2) #if continuous # to ensure converge over R^n.
+    pi = 1/np.sqrt(2*np.pi)*np.exp(-np.sum(x**2)/2)
     return pi
 
 def proposed_prob_And_perturb_config(configObj):
     n = configObj.length
     proposed_config = None
-    g = None
+    gCurrentToProposed = None
+    gProposedToCurrent = None
     g_symmetric = None
     
     #Note if g is symmetric probability density function, then g(x|y)=g(y|x) and the computation of g is not needed actually.
@@ -55,38 +64,41 @@ def proposed_prob_And_perturb_config(configObj):
     #####The discrete probability distribution is as######################################
     #####First pick the number of positions will be flipped, labeled by num###############
     #####Pick all num locations in a config for flipping a sign.##########################
+    """
     proposed_config = configObj.config
     num = np.random.randint(low=0, high = n)+1
     positions =list(range(n))
     np.random.shuffle(positions)
-    [proposed_config[pos]=-1*proposed_config[pos] for pos in positions[0:num]]
+    for pos in positions[0:num]:
+        proposed_config[pos]=-1*proposed_config[pos]
     gCurrentToProposed = 1/n*(1/cnr(n = n,r = num));
     gProposedToCurrent = 1/n*(1/cnr(n = n,r = num));                  
     g_symmetric = True
     """
+    """
     ######discrete uniform probability distribution (symmetric)############
     proposed_config = configObj.config
     pos = np.random.randint(low=0, high = n)
-    proposed_config[pos] = np.random.randint(low = 0, high = 2)*2-1
+    proposed_config[pos] += np.random.randint(low = 0, high = 2)*2-1
     gCurrentToProposed = 1/2*1/n;
     gProposedToCurrent = 1/2*1/n;
     g_symmetric = True
     """
     
-    """
+    
     ######continuous uniform probability distribution (symmetric)############
     proposed_config = configObj.config
     pos = np.random.randint(low=0, high = n)
-    proposed_config[pos] = np.random.randint(low = 0, high = 2)*2-1
+    proposed_config[pos] =  proposed_config[pos] + np.random.randint(low = 0, high = 2)*2-1
     gCurrentToProposed = 1;
     gProposedToCurrent = 1;
     g_symmetric = True
-    """
+    
     
     """
     ######discrete random walk distribution (symmetric)############
     perturb = np.random.randint(low = -1, high = 2, size = n)
-    proposed_config = proposed_config + np.random.randint(low = 0, high = 2, size = n)*2-1
+    proposed_config = configObj.config + np.random.randint(low = 0, high = 2, size = n)*2-1
     gCurrentToProposed = 1/2**n
     gProposedToCurrent = 1/2**n;
     g_symmetric = True
@@ -102,45 +114,49 @@ def proposed_prob_And_perturb_config(configObj):
     g_symmetric = True
     """
     
-    return proposed_config, g_symmetric, gCurrentToProposed, gProposedToCurrent;
+    proposed_configObj = configuration(proposed_config)
+    return proposed_configObj, g_symmetric, gCurrentToProposed, gProposedToCurrent;
 
     
-def ratio_probability(current_pi, proposed_pi, g_symmetric=True, current_g=None, proposed_g=None):
+def ratio_probability(current_pi, proposed_pi, g_symmetric=True, gCurrentToProposed=None, gProposedToCurrent=None):
     if g_symmetric:
-        ratio_prob = min(1, proposed_pi/current_pi)
+        ratio_prob = min(1, proposed_pi/(current_pi+0.0000001))
     else:
-        ratio_prob = min(1, proposed_pi/current_pi*proposed_g/current_g)
+        ratio_prob = min(1, proposed_pi*gProposedToCurrent/(gCurrentToProposed*current_pi+0.00000001))
     
     return ratio_prob
 
-def run(initial_config, iteration, eqm_itr, seed, denom):
+def run(initial_config, iteration, eqm_itr, seed):
     np.random.seed(seed)
     configObj = configuration(initial_config)
-    stable_const = 0.000000000001
-    seqOfConfigs = np.matrix(shape = (iteration - eqm_itr, initial_config.length))
+    seqOfConfigs = np.ndarray(shape = (iteration - eqm_itr, configObj.length))
     #xValue=[i for i in range(iteration)];
     #yValue=[]
     for itr in range(iteration):
         
-        proposed_config, g_symmetric, _, _ =proposed_prob_And_perturb_config(configObj)
-        current_pi = target_prob(configObj.config)
-        proposed_pi = target_prob(proposed_config)
-        accepted_prob = ratio_probability(current_pi=current_pi, proposed_pi=proposed_pi, g_symmetric=True, current_g=None, proposed_g=None)
+        proposed_configObj, g_symmetric, _, _ =proposed_prob_And_perturb_config(configObj)
+        current_pi = target_prob(configObj)
+        proposed_pi = target_prob(proposed_configObj)
+        accepted_prob = ratio_probability(current_pi=current_pi, proposed_pi=proposed_pi, g_symmetric=True, gCurrentToProposed=None, gProposedToCurrent=None)
         if accepted_prob > np.random.rand():
-            configObj = configuration(proposed_config)
+            configObj = proposed_configObj
         if itr >= eqm_itr:
             seqOfConfigs[itr-eqm_itr] = configObj.config
-                    
-    for i in range(100):
-        print("Markov chain following the target probability density func is")
+    
+    print("Markov chain following the target probability density func is")        
+    for i in range(100):   
         print(seqOfConfigs[i])
     #Calculate expectation, std to verify
     #Generate a Gaussian distribution?
+    empirical_mean = np.mean(seqOfConfigs, axis = 0)
+    empirical_std = np.sqrt(np.mean(np.sum((seqOfConfigs-empirical_mean)**2, axis = 1)))
+    print("empirical_mean", empirical_mean)
+    print("empirical_std", empirical_std)
         
 if __name__ == "__main__":
-    initial_config = np.array([10000,-5000,0.00001,-20]) #user can change the initial configuration state
-    iteration = 60000 #user can change iterations
+    initial_config = np.array([0]) #user can change the initial configuration state
+    iteration = 100000 #user can change iterations
     initial_temperature = 100 #user can change initial_temperature
+    eqm_itr = 98000
     seed = 1314 #user can change the seed number
-    denom = 100 #user can change the decay rate of the temperature
-    run(initial_config, iteration, initial_temperature, seed, denom)
+    run(initial_config, iteration, eqm_itr, seed)
